b867056236fd1dffc04af1ee354b95c1368eb025
Victor Bolaños <viktorb.132@gmail.com>
2018-09-07 19:00:55 -0500

    Finalizado

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 posicionarMenu();
 
-    $(window).scroll(function() {    
-        posicionarMenu();
-    });
+$(window).scroll(function() {    
+    posicionarMenu();
+});
 
-    function posicionarMenu() {
-        var altura_del_header = $('.header').outerHeight(true);
-        var altura_del_menu = $('.navegacion').outerHeight(true);
+function posicionarMenu() {
+    var altura_del_header = $('.header').outerHeight(true);
+    var altura_del_menu = $('.navegacion').outerHeight(true);
 
-        if ($(window).scrollTop() >= altura_del_header){
-            $('.navegacion').addClass('fixed');
-            $('.contenido').css('margin-top', (altura_del_menu) + 'px');
-        } else {
-            $('.navegacion').removeClass('fixed');
-            $('.contenido').css('margin-top', '0');
-        }
+    if ($(window).scrollTop() >= altura_del_header){
+        $('.navegacion').addClass('fixed');
+        $('.contenido').css('margin-top', (altura_del_menu) + 'px');
+    } else {
+        $('.navegacion').removeClass('fixed');
+        $('.contenido').css('margin-top', '0');
     }
+}
